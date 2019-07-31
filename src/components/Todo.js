528a6ef8a0a5d5deaa0fbc8e550d5d114b9d7c1a
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-31 21:30:22 +0100

    Added a data fetch mathod Using axios, get data (toDoDate[key].name) from server and display it

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+
 const todo = props => {
   const [toDoName, setToDoName] = useState('')
   const [toDoList, setToDoList] = useState([])
@@ -8,6 +9,18 @@ const todo = props => {
   const inputChangeHandler = (event) => {
     setToDoName(event.target.value)
   }
+
+  useEffect(() => {
+    axios.get('https://react-hooks-practice-7ab69.firebaseio.com/.json').then(result => {
+      console.log(result)
+      const toDoData = result.data
+      const toDos = []
+      for (const key in toDoData) {
+        toDos.push({id: key, name: toDoData[key].name})
+      }
+      setToDoList(toDos)
+    })
+  })
 
   const toDoAddHandler = () => {
     setToDoList(toDoList.concat(toDoName))
@@ -31,7 +44,7 @@ const todo = props => {
     </button>
     <ul>
       {toDoList.map(todo => (
-        <li key={todo}>{todo}</li>
+        <li key={todo.id}>{todo.name}</li>
       ))}
     </ul>
   </React.Fragment>
