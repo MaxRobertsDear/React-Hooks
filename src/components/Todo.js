4500fe94f126ef3875cdc041ba48a41c6c0c55ff
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-31 21:20:50 +0100

    Used hook to send data via HTTP Sent a toDoName to firedbase server

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from 'react'
-
+import axios from 'axios'
 const todo = props => {
   const [toDoName, setToDoName] = useState('')
   const [toDoList, setToDoList] = useState([])
@@ -11,6 +11,13 @@ const todo = props => {
 
   const toDoAddHandler = () => {
     setToDoList(toDoList.concat(toDoName))
+    axios.post('https://react-hooks-practice-7ab69.firebaseio.com/.json', {name: toDoName})
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   return <React.Fragment>
