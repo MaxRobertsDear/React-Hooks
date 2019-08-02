bda19edd0b96c146c12ef9cb23ff94a22e2f0394
MaxRobertsDear <maxhroberts@gmail.com>
2019-08-02 14:19:09 +0100

    Added second argument to useEffect() useEffect() renders after every render cycle. So if it only has one argument, it will enter an infinite loop. Every time useEffect is called, the state is updated, so the render method is called and so on.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,28 +3,27 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const todo = props => {
-  const [toDoName, setToDoName] = useState('')
-  const [toDoList, setToDoList] = useState([])
+  const [todoName, setTodoName] = useState('')
+  const [todoList, setTodoList] = useState([])
 
   const inputChangeHandler = (event) => {
-    setToDoName(event.target.value)
+    setTodoName(event.target.value)
   }
 
   useEffect(() => {
     axios.get('https://react-hooks-practice-7ab69.firebaseio.com/.json').then(result => {
       console.log(result)
-      const toDoData = result.data
-      const toDos = []
-      for (const key in toDoData) {
-        toDos.push({id: key, name: toDoData[key].name})
+      const todoData = result.data
+      const todos = []
+      for (const key in todoData) {
+        todos.push({id: key, name: todoData[key].name})
       }
-      setToDoList(toDos)
+      setTodoList(todos)
     })
-  })
-
-  const toDoAddHandler = () => {
-    setToDoList(toDoList.concat(toDoName))
-    axios.post('https://react-hooks-practice-7ab69.firebaseio.com/.json', {name: toDoName})
+  }, [todoName])
+  const todoAddHandler = () => {
+    setTodoList(todoList.concat(todoName))
+    axios.post('https://react-hooks-practice-7ab69.firebaseio.com/.json', {name: todoName})
       .then(res => {
         console.log(res)
       })
@@ -36,14 +35,14 @@ const todo = props => {
   return <React.Fragment>
     <input 
       type="text" 
-      placeholder="Todo"
+      placeholder="todo"
       onChange={inputChangeHandler}
-      value={toDoName} />
-    <button type="button" onClick={toDoAddHandler}>
+      value={todoName} />
+    <button type="button" onClick={todoAddHandler}>
       Add
     </button>
     <ul>
-      {toDoList.map(todo => (
+      {todoList.map(todo => (
         <li key={todo.id}>{todo.name}</li>
       ))}
     </ul>
